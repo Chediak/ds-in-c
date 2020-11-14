2a0d9155a8baf25497394147b6835362c2299bd9
chediak <shammcity00@gmail.com>
2020-11-14 01:35:06 -0300

    basics

---
+++ bool-array-puzzle.c
@@ -0,0 +1,16 @@
+void changeToZero(int a[2]) 
+{ 
+a[ a[1] ] = a[ !a[1] ]; 
+} 
+
+int main() 
+{ 
+int a[] = {1, 0}; 
+changeToZero(a); 
+
+printf(" arr[0] = %d \n", a[0]); 
+printf(" arr[1] = %d ", a[1]); 
+getchar(); 
+return 0; 
+} 
+
